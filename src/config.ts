d7a9a66d7e808d58058159ff74810ee93a4a28fd
Stanley <>
2022-11-18 17:50:39 +0800

    init

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+export const SITE = {
+  title: '紙飛行機',
+  author: 'Gongwei',
+  description: '点滴记录 生活感悟',
+  keywords: 'Gongwei,IT,Blog,后端,macOS,Java,生活,随笔',
+  icon: 'https://p0.meituan.net/dpplatform/14a9f8c7f448b0cd97b283920818b7216780.png',
+  pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
+  homePage: 'https://weekly.tw93.fun',
+  blogPage: 'https://tw93.fun/',
+  twitterId: 'HiTw93',
+  githubId: 'tw93',
+  repo: 'tw93/weekly'
+};
+
